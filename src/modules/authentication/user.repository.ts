280aa8329ed135b032c2129732b8823a053d279a
SutantoAdiNugroho <adi.nugroho.an836@gmail.com>
2021-06-07 20:53:37 +0700

    encrypting password while register process

--- src/modules/authentication/user.repository.ts
+++ src/modules/authentication/user.repository.ts
@@ -1,6 +1,11 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/signup-user-in.dto';
 import { SignUpOutput } from './dto/signup-user-out.dto';
+import { getHashPassword } from './helpers/user-pass.encrypt';
 import { User } from './user.entity';
 
 @EntityRepository(User)
@@ -9,13 +14,25 @@ export class UserRepository extends Repository<User> {
     authCredentialsDto: AuthCredentialsDto,
   ): Promise<SignUpOutput> {
     const { username, phone_number, password } = authCredentialsDto;
-    const task = this.create({ username, phone_number, password });
-    await this.save(task);
+    const hashed = await getHashPassword(password);
+
+    const task = this.create({ username, phone_number, password: hashed });
+
+    try {
+      await this.save(task);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException(
+          'Username already registered, try sign up with another username',
+        );
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
 
     const response = new SignUpOutput();
-    response.status = 200;
+    response.status = 201;
     response.message = `User successfully signed up with id ${task.id}`;
-    response.data = task;
 
     return response;
   }
