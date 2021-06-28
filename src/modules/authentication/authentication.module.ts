f058bb452d41ecf61c6ef2c518e3e4cef117412e
SutantoAdiNugroho <adi.nugroho.an836@gmail.com>
2021-06-28 11:52:59 +0700

    initial jwt for securing route

--- src/modules/authentication/authentication.module.ts
+++ src/modules/authentication/authentication.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
+import { JwtStrategy } from './helpers/jwt-acc.strategy';
 import { UserRepository } from './user.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({ secret: 'secret', signOptions: { expiresIn: 3600 } }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
+  providers: [AuthenticationService, JwtStrategy],
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthenticationModule {}
